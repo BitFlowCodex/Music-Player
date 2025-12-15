fab7693d427793f6f70dd5d7ffe84d985d04ab22
BitFlowCodex <230321596+BitFlowCodex@users.noreply.github.com>
2025-12-15 15:02:25 +0100

    Variables added in index.js

---
+++ www/index.js
@@ -0,0 +1,25 @@
+// Cover ID Tags
+const cover_image = document.getElementById("cover-image");
+const artist_name = document.getElementById("artist-name");
+const song_name = document.getElementById("song-name");
+
+// Button ID Tags
+const backward_button = document.getElementById("backward");
+const stop_button = document.getElementById("stop");
+const forward_button = document.getElementById("forward");
+
+// Song ID Tags
+const current_timestamp = document.getElementById("current");
+const overall_timestamp = document.getElementById("overall");
+const timestamp_range = document.getElementById("timestamp-range");
+
+// Audio ID Tags
+const volume_range = document.getElementById("volume-range");
+const volume_button = document.getElementById("volume-button");
+
+// Overall Variables
+const audio_url = "";
+const audio = new Audio(audio_url);
+
+
+
